f66e5c8e8ff1e1ccd7c69d96d1a9f75371622f5c
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2022-09-17 10:14:04 +0200

    maplibre & map gl setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import MapGL from "react-map-gl";
+import maplibregl from "maplibre-gl";
+
+import "maplibre-gl/dist/maplibre-gl.css";
 
 function App() {
+  const [interactiveLayers] = useState<string[]>([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MapGL
+      reuseMaps
+      style={{ minHeight: "100%", minWidth: "100%" }}
+      mapLib={maplibregl}
+      mapStyle={`${process.env.PUBLIC_URL}/mapStyle.json`}
+      interactiveLayerIds={interactiveLayers}
+    />
   );
 }
 
