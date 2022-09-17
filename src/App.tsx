647a16d1f4b5f354563d77f16246c7499e9c83cc
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2022-09-17 17:38:53 +0200

    display underlaying features

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import React, { useCallback, useMemo, useState } from "react";
 import MapGL, { MapboxEvent, NavigationControl } from "react-map-gl";
 import maplibregl, { Map, LayerSpecification } from "maplibre-gl";
+import { MapLayerMouseEvent } from "mapbox-gl";
 
 import "maplibre-gl/dist/maplibre-gl.css";
 import { INITIAL_VIEW } from "./config";
 import { Maybe } from "./types";
 import { LayersControl } from "./LayersControl";
-import { isNotNull } from "./lib";
+import { isNotNull, isNotUndefined, isNull } from "./lib";
 import { useEffect } from "react";
+import { Popup } from "./Popup";
+
+// jerusalem hash #11/31.77/35.21/0/0
+// szczecin hash #11/53.42/14.55/0/0
 
 function App() {
   const [map, setMap] = useState<Maybe<Map>>(null);
 
   const [refreshToken, setRefreshToken] = useState<number>(Date.now());
   const [mapLayers, setMapLayers] = useState<LayerSpecification[]>([]);
-  const [touchLayer, setTouchLayer] = useState("");
+  const [features, setFeatures] = useState("");
 
   const interactiveLayersIds = useMemo(
     () => mapLayers.map((layer) => layer.id),
@@ -28,6 +33,42 @@ function App() {
 
   const handleChange = useCallback(() => setRefreshToken(Date.now()), []);
 
+  const queryRendererFeatures = useCallback(
+    (event: MapLayerMouseEvent) => {
+      if (isNull(map) || !event.originalEvent.shiftKey) {
+        setFeatures('')
+        return;
+      }
+      // @ts-ignore comment
+      const features = map.queryRenderedFeatures(event.point).shift();
+
+      if (isNotUndefined(features)) {
+        const displayProperties = [
+          "type",
+          "properties",
+          "id",
+          "layer",
+          "source",
+          "sourceLayer",
+          "state",
+        ];
+
+        const displayFeatures = Object.entries(features).reduce(
+          (acc, [key, value]) => {
+            if (displayProperties.includes(key)) {
+              acc[key] = value;
+            }
+            return acc;
+          },
+          {} as Record<string, unknown>
+        );
+
+        setFeatures(JSON.stringify(displayFeatures, null, 2));
+      }
+    },
+    [map]
+  );
+
   useEffect(() => {
     if (isNotNull(map)) {
       setMapLayers(map.getStyle().layers);
@@ -37,15 +78,18 @@ function App() {
   return (
     <MapGL
       reuseMaps
+      hash
       cursor="crosshair"
       onLoad={handleMapLoad}
       initialViewState={INITIAL_VIEW}
       interactiveLayerIds={interactiveLayersIds}
       mapLib={maplibregl}
       mapStyle={`${process.env.PUBLIC_URL}/mapStyle.json`}
+      onMouseMove={queryRendererFeatures}
     >
       <NavigationControl position="bottom-right" />
       <LayersControl map={map} layers={mapLayers} onChange={handleChange} />
+      <Popup features={features} />
     </MapGL>
   );
 }
---
+++ src/Popup.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import style from "./Popup.module.css";
+
+interface PopupProps {
+  features: string;
+}
+
+export function Popup(props: PopupProps) {
+  return (
+    <div className={style.popup}>
+      <pre>{props.features}</pre>
+    </div>
+  );
+}
