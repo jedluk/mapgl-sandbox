6c139df3131d4ad6cc87a3c4a2c6b660f1a67f47
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2022-09-25 19:41:59 +0200

    add map previews

---
+++ src/img/index.ts
@@ -0,0 +1,9 @@
+import darkMatter from './dark_matter.png'
+import liberty from './liberty.png'
+import positron from './positron.png'
+
+export const IMAGES = Object.freeze({
+  liberty,
+  positron,
+  darkMatter
+})
